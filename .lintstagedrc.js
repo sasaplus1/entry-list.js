9fc0e66fa4acf6700c5bf445cdef22fcaa198468
sasaplus1 <sasaplus1@gmail.com>
2021-02-06 20:58:22 +0900

    improve lint-staged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,20 @@
+const path = require('path');
+
 module.exports = {
-  '*.+(js|ts)': ['eslint --cache --ext .js,.ts', 'prettier --check'],
+  '*.+(js|ts)'(filenames) {
+    const args = filenames.map((filename) => `'${filename}'`).join(' ');
+
+    const commands = [
+      `eslint --cache --ext .js,.ts ${args}`,
+      `prettier --check ${args}`
+    ];
+
+    if (filenames.some((filename) => path.extname(filename) === '.ts')) {
+      commands.push('tsc --noEmit');
+    }
+
+    return commands;
+  },
   'package.json': [
     'npx fixpack --dryRun',
     'prettier --check --parser json-stringify'
