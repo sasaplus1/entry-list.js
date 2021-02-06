cd18f6cbfb1eb8e7eae195cba4fdd3fb947b0946
github-action-benchmark <github@users.noreply.github.com>
2021-02-06 16:28:00 +0000

    add Benchmark (benchmarkjs) benchmark result for 22c8a236e413efda40c08cc9e5f3798991cf5f6f

--- benchmark/data.js
+++ benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1612628875865,
+  "lastUpdate": 1612628880145,
   "repoUrl": "https://github.com/sasaplus1/entry-list.js",
   "entries": {
     "Benchmark": [
@@ -31,6 +31,37 @@ window.BENCHMARK_DATA = {
             "range": "±1.09%",
             "unit": "ops/sec",
             "extra": "88 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sasaplus1@gmail.com",
+            "name": "sasaplus1",
+            "username": "sasaplus1"
+          },
+          "committer": {
+            "email": "sasaplus1@gmail.com",
+            "name": "sasaplus1",
+            "username": "sasaplus1"
+          },
+          "distinct": true,
+          "id": "22c8a236e413efda40c08cc9e5f3798991cf5f6f",
+          "message": "add benchmark workflow",
+          "timestamp": "2021-02-07T01:17:42+09:00",
+          "tree_id": "94bdad06f7c4c25cc7806fc2862ab8359ca429e8",
+          "url": "https://github.com/sasaplus1/entry-list.js/commit/22c8a236e413efda40c08cc9e5f3798991cf5f6f"
+        },
+        "date": 1612628879679,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "node-14:entry-list:fixture1",
+            "value": 67555,
+            "range": "±1.73%",
+            "unit": "ops/sec",
+            "extra": "87 samples"
           }
         ]
       }
