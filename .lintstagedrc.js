f32c4156424d6b3429e4f68068352d8a48abb1fe
sasaplus1 <sasaplus1@gmail.com>
2021-02-06 20:36:08 +0900

    update lint-staged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,8 @@
 module.exports = {
-  'package.json': 'npx fixpack --dryRun',
+  '*.+(js|ts)': ['eslint --cache --ext .js,.ts', 'prettier --check'],
+  'package.json': [
+    'npx fixpack --dryRun',
+    'prettier --check --parser json-stringify'
+  ],
   'package-lock.json': 'node -e "process.exit(1);"'
 };
