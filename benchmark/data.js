cbdfda1dc1a7738b6651608a57041ba3b91b1f4c
github-action-benchmark <github@users.noreply.github.com>
2021-03-06 07:42:52 +0000

    add Benchmark (benchmarkjs) benchmark result for 7c13e62886b29c707b87c6351a064ead22aa32f0

--- benchmark/data.js
+++ benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1615016569316,
+  "lastUpdate": 1615016572242,
   "repoUrl": "https://github.com/sasaplus1/entry-list.js",
   "entries": {
     "Benchmark": [
@@ -93,6 +93,37 @@ window.BENCHMARK_DATA = {
             "range": "±0.91%",
             "unit": "ops/sec",
             "extra": "87 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sasaplus1@gmail.com",
+            "name": "sasaplus1",
+            "username": "sasaplus1"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7c13e62886b29c707b87c6351a064ead22aa32f0",
+          "message": "Merge pull request #1 from sasaplus1/optimization\n\noptimization",
+          "timestamp": "2021-03-06T16:42:05+09:00",
+          "tree_id": "226d6246727ded752cd72faaf93e90b8b926020b",
+          "url": "https://github.com/sasaplus1/entry-list.js/commit/7c13e62886b29c707b87c6351a064ead22aa32f0"
+        },
+        "date": 1615016571775,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "node-14:entry-list:fixture1",
+            "value": 71462,
+            "range": "±1.46%",
+            "unit": "ops/sec",
+            "extra": "86 samples"
           }
         ]
       }
